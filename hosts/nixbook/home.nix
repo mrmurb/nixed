9a3ad8c497035581bd49247d0687a75bfa424db6
mrmurb <max@ig4.se>
2021-05-17 23:25:49 +0200

    we use bspwm :tada:

--- hosts/nixbook/home.nix
+++ hosts/nixbook/home.nix
@@ -8,12 +8,49 @@
     enable = true;
     windowManager = {
       i3 = {
-        enable = true;
+        enable = false;
         package = pkgs.i3-gaps;
         config = {
           modifier = "Mod4";
           terminal = "kitty";
         };
+      };
+      bspwm = {
+        enable = true;
+        startupPrograms = [ "kitty" ];
+        settings = {
+          border_width = 2;
+          window_gap = 3;
+          split_ratio = 0.5;
+          borderless_monocle = true;
+          gapless_monocle = true;
+          focus_by_distance = true;
+          focus_follows_pointer = true;
+          pointer_modifier = "mod4";
+          pointer_action1 = "move";
+          pointer_action2 = "resize_corner";
+        };
+        monitors = {
+          eDP1 = [ "0001" "0010" "0011" "0100" ];
+        };
+      };
+    };
+  };
+
+  services = {
+    sxhkd = {
+      enable = true;
+      keybindings = {
+        "mod4 + shift + e" = "bspc quit";
+        "mod4 + shift + q" = "bspc node -c";
+        "mod4 + t"         = "bspc node -l next";
+        "mod4 + b"         = "bspc desktop -B";
+        "mod4 + Tab"       = "bspc node -f last";
+        "mod4 + {a,s,f}"   = "bspc node -t {tiled,floating,fullscreen}";
+        "mod4 + {ctrl,shift,alt} + {Left,Down,Up,Right}" = "bspc node -{f,s,p} {west,south,north,east}";
+        "mod4 + {_,shift + }{1-9,0}"                     = "bspc {desktop -f,node -d} '^{1-9,10}'";
+        "super + {_,shift + } d"  = "rofi -show {run,ssh} -hide-scrollbar";
+        "mod4 + Return"           = "kitty";
       };
     };
   };
@@ -41,6 +78,10 @@
       extraConfig = ''
         colorscheme gruvbox
       '';
+    };
+
+    rofi = {
+      enable = true;
     };
 
     vscode = {
